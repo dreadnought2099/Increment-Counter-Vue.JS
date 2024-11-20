ed654fd4d55dd57ce214beb7ebc92a472b69a62b
dreadnought2099 <raymart.magallanes@bisu.edu.ph>
2024-11-20 10:30:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-container">
+    <h1>Parent Component</h1>
+    <h2>Counter Component</h2>
+    <div>
+      <p>Current count: {{ latestCount }}</p>
+      <Counter @update-count="handleUpdatedCount" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+import "./styles/Counter.css";
+
+export default {
+  name: "App",
+  components: {
+    Counter,
+  },
+  data() {
+    return {
+      latestCount: 0,
+    };
+  },
+  methods: {
+    handleUpdatedCount(count) {
+      this.latestCount = count;
+    },
+  },
+};
+</script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <button class="button1" @click="decrement">Decrement</button>
+      <button class="button2" @click="increment">Increment</button>
+    </div>
+    <div>
+      <p>Current count in parent:</p>
+      <p class="count">{{ count }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+      this.$emit("update-count", this.count);
+    },
+    decrement() {
+      this.count--;
+      this.$emit("update-count", this.count);
+    },
+  },
+};
+</script>
